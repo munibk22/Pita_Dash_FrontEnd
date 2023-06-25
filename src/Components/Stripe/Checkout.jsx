8ee6292678d5c669b61eb37d6c5d7f99c12d43da
munibk22 <munib22@gmail.com>
2023-06-25 00:37:50 -0400

    Define icon sizes

--- src/Components/Stripe/Checkout.jsx
+++ src/Components/Stripe/Checkout.jsx
@@ -1,6 +1,12 @@
 import {PaymentElement} from '@stripe/react-stripe-js';
 
 const Checkout = () => {
+ const icons = [
+  { sizes: '16x16' },
+  { sizes: '32x32' },
+  { sizes: '64x64' },
+  // Add more dictionary entries as needed
+];
   return (
     <form>
       <PaymentElement />
